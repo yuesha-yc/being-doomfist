c0c386db7fef0bfd7d94a66b168639276912d76f
yuesha-yc <kevinwangyichen@gmail.com>
2021-04-18 21:14:22 +0800

    Finished draft

--- calhacks.py
+++ calhacks.py
@@ -14,23 +14,6 @@ from directkeys import PressKey, ReleaseKey, W, A, S, D
 data = []
 
 
-def dataHandler2():
-    global data
-
-    ser = serial.Serial("COM7", 115200)
-
-    while True:
-        recv = ser.readline()
-        recv = recv.replace(b"\n", b"")
-        if not recv or len(recv) != 4:
-            continue
-            
-        data = struct.unpack("HH", recv)
-        data = np.array(data, dtype=np.float64)
-        data *= 1/4096.
-        print(data)
-
-
 def dataHandler3():
     global data
 
@@ -39,14 +22,15 @@ def dataHandler3():
     cnt  = 0 
     while True:
         recv = ser.readline()
+        # print(recv)
         recv = recv.replace(b"\n", b"")
-        if not recv or len(recv) != 6:
+        if not recv or len(recv) != 8:
             continue
             
-        data = struct.unpack("HHH", recv)
+        data = struct.unpack("HHHH", recv)
         data = np.array(data, dtype=np.float64)
         data *= 1/4096.
-        if not cnt % 50:
+        if not cnt % 100:
             print(data)
         cnt += 1
 
@@ -63,48 +47,6 @@ y2s = []
 y3s = []
 
 
-def start2(fig, ax, xs, y1s, y2s):
-    global data
-
-    def animate(i, xs, y1s, y2s):
-        global data
-        if data[0] > 0.3:
-            PressKey(0x19)
-            ReleaseKey(0x19)
-            
-        elif data[1] > 0.5:
-            print("Right Click")
-            PressKey(0x18)
-        else:
-            ReleaseKey(0x18)
-        
-        # Plot
-
-        temp_c = data[0]
-        temp_c2 = data[1]
-
-        xs.append(datetime.datetime.now())
-        y1s.append(temp_c)
-        y2s.append(temp_c2)
-
-        xs = xs[-100:]
-        y1s = y1s[-100:]
-        y2s = y2s[-100:]
-
-        ax.clear()
-        ax.plot(xs, y1s)
-        ax.plot(xs, y2s)
-
-        # Format plot
-        plt.xticks(rotation=45, ha='right')
-        plt.subplots_adjust(bottom=0.30)
-        plt.title('Muscle Voltage')
-        plt.ylabel('Voltage')
-        plt.ylim(0., 1.)
-
-    ani = animation.FuncAnimation(fig, animate, fargs=(xs, y1s, y2s), interval=0.001)
-    plt.show()
-
 
 def start3(fig, ax, xs, y1s, y2s, y3s):
     global data
@@ -116,7 +58,7 @@ def start3(fig, ax, xs, y1s, y2s, y3s):
             PressKey(0x19)
             ReleaseKey(0x19)
 
-        elif data[2] > 0.7:
+        elif data[2] > 0.6:
             PressKey(0x12)
             ReleaseKey(0x12)
             
@@ -124,6 +66,12 @@ def start3(fig, ax, xs, y1s, y2s, y3s):
             PressKey(0x18)
         else:
             ReleaseKey(0x18)
+        
+        key = 0x11 # 0x17
+        if not data[3]:
+            PressKey(key)
+        else:
+            ReleaseKey(key)
         
         # Plot
 
@@ -153,7 +101,7 @@ def start3(fig, ax, xs, y1s, y2s, y3s):
         plt.ylabel('Voltage')
         plt.ylim(0., 1.)
 
-    ani = animation.FuncAnimation(fig, animate, fargs=(xs, y1s, y2s, y3s), interval=0.005)
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs, y1s, y2s, y3s), interval=100)
     plt.show()
 
 start3(fig, ax, xs, y1s, y2s, y3s)
